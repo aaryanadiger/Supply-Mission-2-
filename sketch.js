28d17dd2933e9719dbdf4c64c627d86be16f9194
aaryanadiger <aaryanadiger@gmail.com>
2021-02-26 12:22:17 +0530

    update

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup() {
 	rectMode(CENTER);
 	
 
-	packageSprite=createSprite(width/2, 80, 10,10);
+	packageSprite=createSprite(width/2, 80);
 	packageSprite.addImage(packageIMG)
 	packageSprite.scale=0.2
 
@@ -32,12 +32,12 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 25 , {restitution:0.5, isStatic:true});
 	World.add(world, packageBody);
 	
 
 	
-	ground = Bodies.rectangle(width/2, 622, width, 10 , {isStatic:true} );
+	ground = Bodies.rectangle(width/2, height - 35 , width, 10 , {isStatic:true} );
  	World.add(world, ground);
 
 
